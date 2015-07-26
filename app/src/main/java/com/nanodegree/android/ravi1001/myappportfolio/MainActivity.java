fe092d2c7883e7e23ad78d3a19aa88d30f1210be
Ravi <ravi10010101@gmail.com>
2015-07-26 18:15:18 +0530

    feat: Add code to show toasts when buttons are clicked

--- app/src/main/java/com/nanodegree/android/ravi1001/myappportfolio/MainActivity.java
+++ app/src/main/java/com/nanodegree/android/ravi1001/myappportfolio/MainActivity.java
@@ -4,14 +4,26 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the on click event handler for all buttons.
+        ((Button)findViewById(R.id.btn_spotify_streamer_app)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_scores_app)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_library_app)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_build_it_bigger_app)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_xyz_reader_app)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_capstone_app)).setOnClickListener(this);
+
     }
 
     @Override
@@ -34,5 +46,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Handles button click events. Presently, displays a toast corresponding to the button
+     * clicked. Later, once the project apps are ready, it will launch these apps from here.
+     */
+    @Override
+    public void onClick(View v) {
+        // Get the view Id.
+        int viewId = v.getId();
+
+        // Determine the button clicked and display appropriate toast.
+        switch (viewId) {
+            case R.id.btn_spotify_streamer_app: {
+                Toast.makeText(this, R.string.toast_spotify_streamer_app, Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            case R.id.btn_scores_app: {
+                Toast.makeText(this, R.string.toast_scores_app, Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            case R.id.btn_library_app: {
+                Toast.makeText(this, R.string.toast_library_app, Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            case R.id.btn_build_it_bigger_app: {
+                Toast.makeText(this, R.string.toast_build_it_bigger_app, Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            case R.id.btn_xyz_reader_app: {
+                Toast.makeText(this, R.string.toast_xyz_reader_app, Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            case R.id.btn_capstone_app: {
+                Toast.makeText(this, R.string.toast_capstone_app, Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
     }
 }
