93e2f5211fe7f0a5b0a52aaaeada5f0f92564a77
Ravi <ravi10010101@gmail.com>
2016-01-10 12:35:10 +0530

    feat: Add buttons corresponding to new apps in the portfolio

--- app/src/main/java/com/nanodegree/android/ravi1001/myappportfolio/MainActivity.java
+++ app/src/main/java/com/nanodegree/android/ravi1001/myappportfolio/MainActivity.java
@@ -1,13 +1,26 @@
+/*
+ * Copyright (C) 2015 Ravi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nanodegree.android.ravi1001.myappportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,40 +30,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         // Set the on click event handler for all buttons.
-        ((Button)findViewById(R.id.btn_spotify_streamer_app)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_scores_app)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_library_app)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_build_it_bigger_app)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_xyz_reader_app)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_capstone_app)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_popular_movies)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_alexandria)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_football_scores)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_build_it_bigger)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_xyz_reader)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_news_bytes)).setOnClickListener(this);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
-     * Handles button click events. Presently, displays a toast corresponding to the button
-     * clicked. Later, once the project apps are ready, it will launch these apps from here.
+     * Handles button click events and launches the app clicked on.
      */
     @Override
     public void onClick(View v) {
@@ -59,32 +49,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Determine the button clicked and display appropriate toast.
         switch (viewId) {
-            case R.id.btn_spotify_streamer_app: {
+            case R.id.btn_popular_movies: {
                 Toast.makeText(this, R.string.toast_spotify_streamer_app, Toast.LENGTH_SHORT).show();
                 break;
             }
 
-            case R.id.btn_scores_app: {
+            case R.id.btn_alexandria: {
                 Toast.makeText(this, R.string.toast_scores_app, Toast.LENGTH_SHORT).show();
                 break;
             }
 
-            case R.id.btn_library_app: {
+            case R.id.btn_football_scores: {
                 Toast.makeText(this, R.string.toast_library_app, Toast.LENGTH_SHORT).show();
                 break;
             }
 
-            case R.id.btn_build_it_bigger_app: {
+            case R.id.btn_build_it_bigger: {
                 Toast.makeText(this, R.string.toast_build_it_bigger_app, Toast.LENGTH_SHORT).show();
                 break;
             }
 
-            case R.id.btn_xyz_reader_app: {
+            case R.id.btn_xyz_reader: {
                 Toast.makeText(this, R.string.toast_xyz_reader_app, Toast.LENGTH_SHORT).show();
                 break;
             }
 
-            case R.id.btn_capstone_app: {
+            case R.id.btn_news_bytes: {
                 Toast.makeText(this, R.string.toast_capstone_app, Toast.LENGTH_SHORT).show();
                 break;
             }
