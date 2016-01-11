9a195b895eba0d110a43a0239fcbe79e27d2ff87
Ravi <ravi10010101@gmail.com>
2016-01-11 16:16:10 +0530

    feat: Add code to launch news bytes app

--- app/src/main/java/com/nanodegree/android/ravi1001/myappportfolio/MainActivity.java
+++ app/src/main/java/com/nanodegree/android/ravi1001/myappportfolio/MainActivity.java
@@ -16,6 +16,7 @@
 
 package com.nanodegree.android.ravi1001.myappportfolio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,60 +25,96 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // Reference to all the buttons to launch the various apps.
+    private Button mModernArtBtn;
+    private Button mPopularMoviesBtn;
+    private Button mAlexandriaBtn;
+    private Button mFootballScoresBtn;
+    private Button mBuildItBiggerBtn;
+    private Button mXYZReaderBtn;
+    private Button mGoUbiquitousBtn;
+    private Button mNewsBytesBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set the on click event handler for all buttons.
-        ((Button)findViewById(R.id.btn_popular_movies)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_alexandria)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_football_scores)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_build_it_bigger)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_xyz_reader)).setOnClickListener(this);
-        ((Button)findViewById(R.id.btn_news_bytes)).setOnClickListener(this);
+        // Get the references to each button.
+        mModernArtBtn = (Button) findViewById(R.id.btn_modern_art);
+        mPopularMoviesBtn = (Button) findViewById(R.id.btn_popular_movies);
+        mAlexandriaBtn = (Button) findViewById(R.id.btn_alexandria);
+        mFootballScoresBtn = (Button) findViewById(R.id.btn_football_scores);
+        mBuildItBiggerBtn = (Button) findViewById(R.id.btn_build_it_bigger);
+        mXYZReaderBtn = (Button) findViewById(R.id.btn_xyz_reader);
+        mGoUbiquitousBtn = (Button) findViewById(R.id.btn_go_ubiquitous);
+        mNewsBytesBtn = (Button) findViewById(R.id.btn_news_bytes);
 
+        // Set the on click event handler for each button.
+        mModernArtBtn.setOnClickListener(this);
+        mPopularMoviesBtn.setOnClickListener(this);
+        mAlexandriaBtn.setOnClickListener(this);
+        mFootballScoresBtn.setOnClickListener(this);
+        mBuildItBiggerBtn.setOnClickListener(this);
+        mXYZReaderBtn.setOnClickListener(this);
+        mGoUbiquitousBtn.setOnClickListener(this);
+        mNewsBytesBtn.setOnClickListener(this);
     }
 
     /**
-     * Handles button click events and launches the app clicked on.
+     * Launches the app corresponding to the button clicked on.
      */
     @Override
     public void onClick(View v) {
         // Get the view Id.
         int viewId = v.getId();
 
-        // Determine the button clicked and display appropriate toast.
+        // Determine the button clicked and launch the corresponding app.
         switch (viewId) {
+            case R.id.btn_modern_art: {
+                break;
+            }
             case R.id.btn_popular_movies: {
-                Toast.makeText(this, R.string.toast_spotify_streamer_app, Toast.LENGTH_SHORT).show();
                 break;
             }
-
             case R.id.btn_alexandria: {
-                Toast.makeText(this, R.string.toast_scores_app, Toast.LENGTH_SHORT).show();
                 break;
             }
-
             case R.id.btn_football_scores: {
-                Toast.makeText(this, R.string.toast_library_app, Toast.LENGTH_SHORT).show();
                 break;
             }
-
             case R.id.btn_build_it_bigger: {
-                Toast.makeText(this, R.string.toast_build_it_bigger_app, Toast.LENGTH_SHORT).show();
                 break;
             }
-
             case R.id.btn_xyz_reader: {
-                Toast.makeText(this, R.string.toast_xyz_reader_app, Toast.LENGTH_SHORT).show();
                 break;
             }
-
+            case R.id.btn_go_ubiquitous: {
+                break;
+            }
             case R.id.btn_news_bytes: {
-                Toast.makeText(this, R.string.toast_capstone_app, Toast.LENGTH_SHORT).show();
+                // Launch the news bytes app.
+                launchApp(getString(R.string.package_news_bytes));
                 break;
             }
+        }
+    }
+
+    /**
+     * Launches the app whose fully qualified package name is specified.
+     */
+    private void launchApp(String packageName) {
+        // Get the launch intent from the package manager for the specified package.
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage(packageName);
+
+        // Check if the app is installed on the device.
+        if(launchIntent != null) {
+            // Launch the app.
+            launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(launchIntent);
+        } else {
+            // Show error message to user.
+            Toast.makeText(this, getString(R.string.msg_err_app_not_found), Toast.LENGTH_LONG).show();
         }
     }
 }
